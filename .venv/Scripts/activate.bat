886a54a562af1a807660a7f8734a9c19b6f7a183
ernesto-teixeira-silva <ernesto.silva@excel4me.onmicrosoft.com>
2024-02-13 20:51:52 +0000

    Correção dos ficheiros do ambiente ambiente virtual

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ernes\OneDrive\Ambiente de Trabalho\eleicao_deputados\.venv
+set VIRTUAL_ENV=C:\Users\ernes\OneDrive\Ambiente de Trabalho\eleicoes_legislativas\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
